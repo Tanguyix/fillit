fd51086444ea3fa41064e44672e4057389141a8c
Tanguy BOISSEL-DALLIER <tboissel@e1r3p14.42.fr>
2018-04-16 11:47:10 +0200

    Un truc presque utile

--- ft_fillit.h
+++ ft_fillit.h
@@ -6,7 +6,7 @@
 /*   By: twalpert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 16:35:19 by twalpert          #+#    #+#             */
-/*   Updated: 2018/04/16 11:33:48 by tboissel         ###   ########.fr       */
+/*   Updated: 2018/04/16 11:46:48 by tboissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,15 @@ typedef struct		s_fill
 	int				*tpos;
 	int				dim;
 }					t_fill;
+
+
+typedef struct		s_count
+{
+	int				line;
+	int				ch;
+	int				hash;
+}					t_count;
+
 
 t_fill				ft_read_check(int fd, t_fill tetroes);
 void				ft_usage(void);
